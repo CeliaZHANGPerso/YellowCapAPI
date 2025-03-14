6f5de4774027ed471695221cf95f6187797523b9
Anne Baril <barilanne076@gmail.com>
2025-03-14 10:42:17 +0000

    create project structure

---
+++ api.py
@@ -0,0 +1,14 @@
+import fastapi
+import uvicorn
+
+from fastapi import FastAPI
+from uvicorn import Config, Server
+from yellowcab.predict import predict
+
+app = FastAPI()
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
+#@app.get("/predict")    
