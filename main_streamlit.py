b6300d96462d407765386b28121e823c6a5fec11
Celia ZHANG <celiazhang349@gmail.com>
2025-03-18 13:27:35 +0000

    delete useless stcode

--- main_streamlit.py
+++ main_streamlit.py
@@ -3,14 +3,6 @@ import pandas as pd
 import numpy as np
 import requests
 
-st.title("Yellow Cab")
-st.header("Estimate the duration")
-st.markdown("This is the markdown")
-st.subheader("This is the subheader")
-st.caption("This is the caption")
-st.code("x = 2021")
-st.latex(r''' a+a r^1+a r^2+a r^3 ''')
-
 st.number_input("VendorID", value=1)
 st.number_input("passenger_count", value=1)
 st.number_input("trip_distance", value=2.1)
