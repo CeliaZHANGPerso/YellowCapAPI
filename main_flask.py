ea566e06bbaa19010b95939e2022a53f5dfeaa6a
Celia ZHANG <celiazhang349@gmail.com>
2025-03-18 15:03:40 +0000

    flask api front using gcp cloud run works correctly

---
+++ main_flask.py
@@ -0,0 +1,62 @@
+from flask import request, Flask, render_template
+import requests
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "Yellow Cab Prediction API"
+
+@app.route('/predict', methods=['GET','POST'])
+def predict():
+    if request.method == 'POST':
+        VendorID = request.form['VendorID']
+        passenger_count = request.form['passenger_count']
+        trip_distance = request.form['trip_distance']
+        RatecodeID = request.form['RatecodeID']
+        store_and_fwd_flag = request.form['store_and_fwd_flag']
+        PULocationID = request.form['PULocationID']
+        DOLocationID = request.form['DOLocationID']
+        payment_type = request.form['payment_type']
+        fare_amount = request.form['fare_amount']
+        extra = request.form['extra']
+        mta_tax = request.form['mta_tax']
+        tip_amount = request.form['tip_amount']
+        tolls_amount = request.form['tolls_amount']
+        improvement_surcharge = request.form['improvement_surcharge']
+        total_amount = request.form['total_amount']
+        congestion_surcharge = request.form['congestion_surcharge']
+        airport_fee = request.form['airport_fee']
+
+        input_dict = {
+            "VendorID": [VendorID],
+            "passenger_count": [passenger_count],
+            "trip_distance": [trip_distance],
+            "RatecodeID": [RatecodeID],
+            "store_and_fwd_flag": [store_and_fwd_flag],
+            "PULocationID": [PULocationID],
+            "DOLocationID": [DOLocationID],
+            "payment_type": [payment_type],
+            "fare_amount": [fare_amount],
+            "extra": [extra],
+            "mta_tax": [mta_tax],
+            "tip_amount": [tip_amount],
+            "tolls_amount": [tolls_amount],
+            "improvement_surcharge": [improvement_surcharge],
+            "total_amount": [total_amount],
+            "congestion_surcharge": [congestion_surcharge],
+            "airport_fee": [airport_fee]
+        }
+        BASE_URL =  'https://yellowcapapi-609285842864.europe-west9.run.app'
+        prediction = requests.get(f'{BASE_URL}/predict_get/', params=input_dict)
+        print(prediction.json())
+        return render_template('predict.html', prediction=prediction.json())
+    return render_template('predict.html')
+
+
+if __name__ == "__main__":
+    app.run(
+        host='0.0.0.0',
+        port=8004,
+        debug=True
+    )
